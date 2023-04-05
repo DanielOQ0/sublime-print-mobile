ad31f23f0e9100581ab04965d5af7a586f6c0edb
IanFeugeas <ianfabricio1889@gmail.com>
2023-04-05 20:02:21 -0300

    fix

---
+++ src/navigation/DrawerNavigation.jsx
@@ -0,0 +1,12 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, {useState} from 'react';
+import { useFocusEffect } from '@react-navigation/core';
+import { useSelector } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Index from '../screens/Index';
+
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigation() {
+
+}
